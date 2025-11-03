f7f5015ee2f077e1d15a65f92379902636f63ab2
happ-2h <hikonetgames@gmail.com>
2025-11-02 17:55:23 -0800

    Game constants created

---
+++ src/game/constants.js
@@ -0,0 +1,13 @@
+// Game screen
+export const GAME_WIDTH        = 128;
+export const GAME_HEIGHT       =  72;
+export const GAME_WIDTH_TILES  =  16;
+export const GAME_HEIGHT_TILES =   9;
+
+// Game constants
+export const SCALE     = 5;
+export const TILE_SIZE = 8;
+
+// Canvas screen
+export const SCREEN_WIDTH  = GAME_WIDTH  * SCALE;
+export const SCREEN_HEIGHT = GAME_HEIGHT * SCALE;
