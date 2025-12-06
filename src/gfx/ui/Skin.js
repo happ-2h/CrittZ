7bf5573edf082766892eea5e0398faedff7a5404
happ-2h <hikonetgames@gmail.com>
2025-12-06 12:43:34 -0800

    Skin - preview functionality

--- src/gfx/ui/Skin.js
+++ src/gfx/ui/Skin.js
@@ -226,6 +226,118 @@ class _Skin {
     if (money > 999) money = 999;
     this.#txtCoins.text = money.toString().padStart(3, '0');
   }
+
+  /**
+   * @brief Previews a skin
+   *
+   * @param {Number} n     - Skin number
+   * @param {Number} price - Price of skin
+   */
+  preview(n=0, price=50) {
+    // Top bar
+    new Icon(
+      new Rectangle(
+        0, n<<5,
+        128, 8
+      ),
+      new Rectangle(
+        0, 0, 128, 8
+      )
+    ).draw("skins");
+
+    // Bottom background
+    new Icon(
+      new Rectangle(
+        0, (n<<5) + 8,
+        192, 32
+      ),
+      new Rectangle(
+        0, 56, 192, 32
+      )
+    ).draw("skins");
+
+    // Button left
+    new Icon(
+      new Rectangle(
+        128, n<<5,
+        16, 16
+      ),
+      new Rectangle(
+        0, 56, 16, 16
+      )
+    ).draw("skins");
+
+    // Button right
+    new Icon(
+      new Rectangle(
+        144, n<<5,
+        16, 16
+      ),
+      new Rectangle(
+        16, 56, 16, 16
+      )
+    ).draw("skins");
+
+    // Button pause
+    new Icon(
+      new Rectangle(
+        160, n<<5,
+        16, 16
+      ),
+      new Rectangle(
+        56, 56, 16, 16
+      )
+    ).draw("skins");
+
+    // Button action
+    new Icon(
+      new Rectangle(
+        176, n<<5,
+        16, 16
+      ),
+      new Rectangle(
+        112, 56, 16, 16
+      )
+    ).draw("skins");
+
+    // Text level
+    new Text(
+      "001",
+      new Vec2D(8, 0)
+    ).draw("skins");
+
+    // Text price
+    new Text(
+      `0${price}`,
+      new Vec2D(104, 0)
+    ).draw("skins");
+
+    // Text time
+    new Text(
+      "60",
+      new Vec2D(56, 0)
+    ).draw("skins");
+
+    // Health bar
+    new Icon(
+      new Rectangle(
+        0, 24, 32, 8
+      ),
+      new Rectangle(
+        96, 8, 32, 8
+      )
+    ).draw("skins");
+
+    // Health fill
+    new Icon(
+      new Rectangle(
+        32, 24, 8, 8
+      ),
+      new Rectangle(
+        104, 8, 24, 8
+      )
+    ).draw("skins");
+  }
 };
 
 const Skin = new _Skin;
